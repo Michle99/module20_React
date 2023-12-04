42a4f566425cda002e930ccdb22ad414195ffba4
Michle99 <sammiyonyx@gmail.com>
2023-12-04 10:17:02 -0500

    CleanUp in HomePage.js

--- ALAB_320H_11_React_Page_Layout/src/HomePage.js
+++ ALAB_320H_11_React_Page_Layout/src/HomePage.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Header from './Header';
 import SearchBar from './SearchBar';
 import EmployeeList from './EmployeeList';
-import Footer from './Footer';
 
 const Homepage = () => {
   // State for employee data
